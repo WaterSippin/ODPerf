0f8dc8d44fcc101476870199ec8211ea7669acf2
WaterSippin <103482683+WaterSippin@users.noreply.github.com>
2023-04-28 20:44:34 -0400

    removed non-temp files

--- Installs/Initial-Install.ps1
+++ Installs/Initial-Install.ps1
@@ -1,18 +1,9 @@
 Set-ExecutionPolicy Bypass
 choco install chocolateygui --pre -y
-choco install bleachbit --version 4.4.2 -y
-choco install autoruns --version 14.09 -y
-choco install bulk-crap-uninstaller --version 5.5 -y
-choco install epicgameslauncher --version 1.3.51.0 -y
-choco install littleregistrycleaner --version 1.6.0.20141201 -y
-choco install memreduct --version 3.4 -y
-choco install msiafterburner --version 4.6.5.230424 -y
-choco install everything --version 1.4.11022 -y
-choco install vlc-nightly --version 4.0.0.20230428 -y
-choco install 7zip.portable --version 22.1 -y
-choco install tcpoptimizer --version 3.0.8 -y
-choco install plasso --version 9.0.0.456 -y
-choco install wisemo --version 3.51 -y
-choco install ddu --version 18.0.6.2023012901 -y
-choco install vcredist140 --version 14.34.31938 -y
-choco install geforce-experience --version 3.27.0.112 -y
+choco install autoruns -y
+choco install bulk-crap-uninstaller -y
+choco install tcpoptimizer -y
+choco install plasso -y
+choco install ddu -y
+choco install vcredist140 -y
+choco install geforce-experience -y
